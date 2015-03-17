9aa0e50936a431027504744cb14d98f1127567fe
Adrien <adr.fortune@gmail.com>
2015-03-17 22:21:15 +0100

    Fix connexion à Doctrine via le parameters.php

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -5,3 +5,9 @@ $container->setParameter('database_name', getenv('MYSQL_DATABASE'));
 $container->setParameter('database_user', getenv('MYSQL_USER'));
 $container->setParameter('database_password', getenv('MYSQL_PASSWD'));
 $container->setParameter('secret', getenv('SECRET_KEY'));
+$container->loadFromExtension('doctrine', array('dbal' => array(
+    'driver'   => 'pdo_mysql',
+    'dbname'   => '%database_name%',
+    'user'     => '%database_user%',
+    'password' => '%database_password%',
+)));
