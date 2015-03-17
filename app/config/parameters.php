963758cdf6167bb5dfe0ec9889886440515c387a
Adrien <adr.fortune@gmail.com>
2015-03-18 00:03:38 +0100

    Fix référence security.yml > security.php

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -5,17 +5,10 @@ $container->setParameter('database_name', getenv('MYSQL_DATABASE'));
 $container->setParameter('database_user', getenv('MYSQL_USER'));
 $container->setParameter('database_password', getenv('MYSQL_PASSWD'));
 $container->setParameter('secret', getenv('SECRET_KEY'));
+
 $container->loadFromExtension('doctrine', ['dbal' => [
     'driver'   => 'pdo_mysql',
     'dbname'   => '%database_name%',
     'user'     => '%database_user%',
     'password' => '%database_password%',
-]]);
-$container->loadFromExtension('security', ['firewalls' => [
-    'driver' => [
-        'main' => [
-            'remember_me' => [
-                'key' => '%secret%']
-        ]]
-    ,
 ]]);
